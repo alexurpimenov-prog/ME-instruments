4e8cd2d8a78071cc31cc105c725eaaeadada369a
alexurpimenov-prog <alexurpimenov@gmail.com>
2026-08-24 11:57:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -78,7 +78,7 @@ export default function App() {
 
           <Calculator />
 
-          <ButtonCard onClick={() => console.log('Клик!')} />
+          <ButtonCard />
 
           <ProcessFlow
           steps={PROCESS_STEPS}
